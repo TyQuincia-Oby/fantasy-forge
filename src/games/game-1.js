5435512c01ee05fc829bd9a9319f7562c6043499
TyQuincia Oby <tyquinciaoby@gmail.com>
2025-12-18 20:07:10 -0600

    Step 7 Blueprint for Heroes: Crafting the Adventurer Class [complete]

--- src/games/game-1.js
+++ src/games/game-1.js
@@ -1,2 +1,6 @@
 //Adventurer playground
+import { Adventurer } from "../models/Adventurer.js";
+
+const hero1 = new Adventurer("Robin", 80);
+hero1.scout();
 console.log("Fantasy Forge Initialized!")
---
+++ src/models/Adventurer.js
@@ -0,0 +1,12 @@
+class Adventurer{
+    constructor(name,health){
+        this.name = name;
+        this.health = health;
+    }
+
+    scout(){
+        console.log(`${this.name} is scouting. Current health: ${this.health}`)
+    }
+}
+
+export {Adventurer};
